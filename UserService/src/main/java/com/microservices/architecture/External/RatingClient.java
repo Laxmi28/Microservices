5e7a1f5995da20d5e81af1340a252f4f47945509
Known <laxpandey28@gmail.com>
2025-07-27 17:28:38 +0530

    feign cleint - POST usage in rating service

--- UserService/src/main/java/com/microservices/architecture/External/RatingClient.java
+++ UserService/src/main/java/com/microservices/architecture/External/RatingClient.java
@@ -2,6 +2,8 @@ package com.microservices.architecture.External;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import com.microservices.architecture.Entities.Rating;
 
 @FeignClient(name = "RATING-SERVICE")
@@ -9,5 +11,12 @@ public interface RatingClient {
     
      @GetMapping("/rating/{userId}")
      Rating[] getRating(@PathVariable String userId);
+     
+     @PostMapping("/rating/addRating")
+     Rating createRating(Rating rating);
+     
+     /*
+      * similar to create rating we can have put post , delete all together
+      */
     
 } 
--- UserService/src/test/java/com/microservices/architecture/ArchitectureApplicationTests.java
+++ UserService/src/test/java/com/microservices/architecture/ArchitectureApplicationTests.java
@@ -1,7 +1,12 @@
 package com.microservices.architecture;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.microservices.architecture.Entities.Rating;
+import com.microservices.architecture.External.RatingClient;
+
 
 @SpringBootTest
 class ArchitectureApplicationTests {
@@ -9,5 +14,20 @@ class ArchitectureApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+    
+	@Autowired
+	private RatingClient ratingClient;
+
+	@Test 
+	void createRatingUsingFeignClient(){
+       Rating rating = Rating.builder().rating(3)
+	   							.userId(null)
+								.hotelId(null)
+								.feedback("This is just a  test").build();
+
+	  ratingClient.createRating(rating);			
+	  System.out.println("Rating test done");				
+	}
+
 
 }
